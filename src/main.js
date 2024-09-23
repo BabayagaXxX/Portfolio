5d870d38d688abcfc0182c583b7c3620a3122dd1
Arn <almplayda00289@usep.edu.ph>
2024-09-23 10:56:14 +0800

    Initial

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed(".auto-type",{
+    strings:["Web Developer", "UI/UX Designer"],
+    typeSpeed: 150,
+    backspeed:150,
+    loop:true
+})
+/*
+var typed = new Typed(".p", {
+    strings: ["Join me on a digital adventure as I dive into the world of web design and UI/UX. I'm a budding web designer with a passion for creating memorable user experiences. Armed with a mix of technical curiosity and a touch of creativity, I'm on a mission to turn ideas into visually appealing and functional websites."],
+    typeSpeed:20,
+    looped:true
+})*/
+
