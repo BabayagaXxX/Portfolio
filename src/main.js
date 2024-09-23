325172cbaf91f53bddbda05028f380da21b49f3f
BabayagaXxX <almplayda00289@usep.edu.ph>
2024-09-23 11:20:49 +0800

    src and json

--- src/main.js
+++ src/main.js
@@ -9,6 +9,6 @@ var typed = new Typed(".auto-type",{
 var typed = new Typed(".p", {
     strings: ["Join me on a digital adventure as I dive into the world of web design and UI/UX. I'm a budding web designer with a passion for creating memorable user experiences. Armed with a mix of technical curiosity and a touch of creativity, I'm on a mission to turn ideas into visually appealing and functional websites."],
     typeSpeed:20,
-    looped:true
+    looped:trues
 })*/
 
